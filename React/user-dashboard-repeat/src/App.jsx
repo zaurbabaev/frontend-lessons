8c639fc4da86de0cf133c7d25601a9fede382635
Zaur <zaurik87@gmail.com>
2026-01-18 20:57:36 +0400

    create

---
+++ React/user-dashboard-repeat/src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import UserList from "./components/UserList";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100">
+      <Header />
+      <UserList/>
+      <Footer />
+    </div>
+  );
+}
